af94ae391aecb6f66b4aad90904d5e548b146add
Guilherme Cardoso de Almeida <36213764+cardosoGui@users.noreply.github.com>
2018-09-03 11:43:04 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import history from "../src/components/utils/History";
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+registerServiceWorker();
